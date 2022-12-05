ccb122d622214aab893b19584079ed66368405dc
Vinayak Patil <you@example.com>
2022-12-05 22:49:11 +0530

    Models

---
+++ models/admin.Model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+
+exports.adminSchema = mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    default: ADMIN
+  },
+  createdAt: {
+    type: Date,
+    default: Date
+  }
+});
