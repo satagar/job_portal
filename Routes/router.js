2cf82dc3a8aa400931eb5126ffa19acf2b6383e7
unknown <pranitdubal5@gmail.com>
2022-12-07 21:28:27 +0530

    Added

--- Routes/router.js
+++ Routes/router.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const authController = require('../Controllers/auth.controller')
+const findById = require('../Controllers/findById.controller');
+const deleteById = require('../Controllers/deleteById.controller')
 const router = express.Router();
 
-router.post('/job_portal/api/v1/auth/admin')
+router.post('/job_portal/api/v1/auth/signup', authController.signup)
+router.get('/job_portal/api/v1/auth/login', authController.login)
+router.get('/job_portal/api/v1/auth/users/:id', findById.findById);
+router.get('/job_portal/api/v1/auth/users/:id', deleteById.deleteById)
+module.exports = router;
