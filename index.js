2d395aaac513130936d7f3e9947a95a37f12d973
Rahul <raahull24041978@gmail.com>
2022-12-10 19:24:04 +0530

    resolving conflicts

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const EmpHire = express()
+
+mongoose
+    .connect(process.env.MONGO_DB_URI, options)
+    .then(() => console.log('Connected to DataBase'))
+    .catch(err => console.log(err))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+EmpHire.listen(port)
+console.log("App is listening on port: ${port}")
